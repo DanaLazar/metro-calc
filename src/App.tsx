0ba54465e8b87ae22ddf4d1a5e9f5abccc784f97
radu-1 <radutudortel@gmail.com>
2026-03-20 15:09:39 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+import { Button } from '@danalazar/metro-ui'
+
+function App() {
+ 
+
+  return (
+    <div className="text-3xl font-bold underline">
+      <p>some text</p>
+      <Button>
+  Primary Button
+</Button>
+    </div>
+  )
+}
+
+export default App
